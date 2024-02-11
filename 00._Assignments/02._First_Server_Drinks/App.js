308d35037402a9948346485a3d9ff701a7b8a0bc
JesperZamora <jesp78m6@stud.kea.dk>
2024-02-11 17:32:33 +0100

    Rest API - Part 1 / made API design in markdownfile, created new express project and GET endpoints

--- 00._Assignments/02._First_Server_Drinks/App.js
+++ 00._Assignments/02._First_Server_Drinks/App.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const app = express();
 
+let drinks = [];
 
+for(let i = 1; i <= 5; i++) {
+  drinks.push({id: i, name: `name ${i}`, brand: `brand ${i}`});
+}
+ 
+// GET  /drinks
+app.get("/drinks", (req, res) => {
+  res.send(drinks);
+});
 
+// GET  /drinks/{id}
+app.get("/drinks/:id", (req, res) => {
+  const drinkId = +req.params.id;
+  if(isNaN(drinkId)) res.send({data: "id is not correct / number!"})
 
+  const drinkFound = drinks.find(drink => drink.id === drinkId);
+
+  if(!drinkFound) res.send({data: "Not found"});
+  else res.send(drinkFound);
+
+});
+
+// POST   /drinks
+
+// PUT  /drinks/{id}
+
+// DELETE   /drinks/{id}
 
 
 app.listen(8080);

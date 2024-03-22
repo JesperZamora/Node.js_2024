6e8b5d3f0d961391d0a5a83e659313171775ac7e
JesperZamora <jesp78m6@stud.kea.dk>
2024-03-22 13:06:29 +0100

    added something fun to FriendsCounter

--- 07._First_Svelte_Project/src/components/FriendsCounter/FriendsCounter.svelte
+++ 07._First_Svelte_Project/src/components/FriendsCounter/FriendsCounter.svelte
@@ -1,8 +1,11 @@
 <script>
   let numberOfFriends = 0;
+  let cheatInterval;
+  let message = "Dude, you got no friend!";
 
   function incrementFriends() {
     numberOfFriends += 1;
+    systemMessage();
   }
 
   function deleteFriends() {
@@ -10,11 +13,34 @@
   }
 
   function cheat() {
-    setInterval(()=> numberOfFriends += 1, 500);
+    cheatInterval = setInterval(() => {
+      numberOfFriends += 1;
+      systemMessage();
+    }, 100);
   }
-</script> 
+
+  function stopCheat() {
+    clearInterval(cheatInterval);
+  }
+
+  function systemMessage() {
+    switch (numberOfFriends) {
+      case 10:
+        message = "Wuaw, you got friends now!";
+        break;
+
+      case 50:
+        message = "Wtf, you are getting popular!";
+        break;
+      default:
+        break;
+    }
+  }
+</script>
 
 <h2>Fictive friends: {numberOfFriends}</h2>
+<h2>{message}</h2>
 <button on:click={incrementFriends}>Add Friend</button>
 <button on:click={deleteFriends}>Bye Bye Friends</button>
 <button on:click={cheat}>Cheat</button>
+<button on:click={stopCheat}>Stop Cheat</button>
